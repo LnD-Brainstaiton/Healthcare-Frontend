1d19afdd784b3874748de17f8ecdd3cf0a1dcd5e
mesahal <me.sahal2000@gmail.com>
2024-11-25 14:34:16 +0600

    project initialized

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Health_Care_Frontend
+# Getting Started with Create React App
+
+## How to use the template?
+
+-   clone to your local machine
+-   npm install
+-   npm start
+
+## Template Includes
+
+-   react-icons
+-   Eslint
+-   Prettier
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FaYoutube } from 'react-icons/fa';
+
+const App = () => {
+    return (
+        <div>
+            <header>
+                <h1>Health Care</h1>
+            </header>
+            <main>
+            </main>
+            <footer>
+                
+            </footer>
+        </div>
+    );
+};
+
+export default App;
